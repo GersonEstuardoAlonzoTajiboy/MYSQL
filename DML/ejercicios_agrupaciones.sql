fc119775a6026e0a54ffb61f11efbeb49bb11b99
GEAT <gersontajiboy8@gmail.com>
2024-12-27 12:59:39 -0600

    Create ejercicios_agrupaciones.sql

---
+++ DML/ejercicios_agrupaciones.sql
@@ -0,0 +1,14 @@
+USE escuela;
+
+# Ejercicios para ORDER BY
+
+# 1. Cantidad de estudiantes por curso.
+# 2. Promedio de calificaciones por curso.
+# 3. Número de cursos por profesor.
+# 4. Total de calificaciones por curso con más de 3 estudiantes.
+# 5. Promedio de calificaciones en cursos que contienen "Matemáticas".
+# 6. Calificación máxima por curso con ID entre 1 y 3.
+# 7. Nota mínima por profesor en cursos asignados.
+# 8. Promedio de calificaciones por estudiante con nombres que empiezan con 'A'.
+# 9. Cursos con calificaciones promedio mayores a 80.
+# 10. Total de inscripciones por estudiante ordenadas de mayor a menor y limitadas a 5 resultados.
