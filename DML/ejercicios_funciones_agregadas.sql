85c3d0cefcd2861d0702dda5634221ec4af35bfd
GEAT <gersontajiboy8@gmail.com>
2024-12-27 12:59:18 -0600

    Create ejercicios_funciones_agregadas.sql

---
+++ DML/ejercicios_funciones_agregadas.sql
@@ -0,0 +1,31 @@
+USE escuela;
+
+# Ejercicios para COUNT.
+
+# 1. Cantidad de Estudiantes.
+# 2. Cantidad de Cursos Disponibles.
+# 3. Cantidad de Inscripciones en un Curso Específico.
+
+# Ejercicios para SUM.
+
+# 4. Suma de Calificaciones en un Curso.
+# 5. Suma de Calificaciones para Estudiantes con Calificación Mayor a 80.
+# 6. Suma Total de Calificaciones en Todos los Cursos.
+
+# Ejercicios para AVG.
+
+# 7. Promedio de Calificaciones en un Curso Específico.
+# 8. Promedio General de Calificaciones.
+# 9. Promedio de Calificaciones para Estudiantes con Más de 90 Puntos.
+
+# Ejercicios para MIN.
+
+# 10. Calificación Más Baja en un Curso.
+# 11. Nota Mínima General.
+# 12. Nota Más Baja para Estudiantes con Más de 80 Puntos.
+
+# Ejercicios para MAX.
+
+# 13. Calificación Más Alta en un Curso Específico.
+# 14. Nota Máxima General.
+# 15. Nota Más Alta para Estudiantes con Calificación Mayor a 70.
