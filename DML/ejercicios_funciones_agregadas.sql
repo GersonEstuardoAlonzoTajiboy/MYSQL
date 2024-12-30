9990097050fc14615f2dd8922e74abdd3a41025c
GEAT <gersontajiboy8@gmail.com>
2024-12-30 17:06:03 -0600

    Update ejercicios_funciones_agregadas.sql

--- DML/ejercicios_funciones_agregadas.sql
+++ DML/ejercicios_funciones_agregadas.sql
@@ -1,31 +1,41 @@
-USE escuela;
+# Ejercicios con Funciones de Agregación en MySQL
 
-# Ejercicios para COUNT.
+# Ejercicios para COUNT
 
 # 1. Cantidad de Estudiantes.
 # 2. Cantidad de Cursos Disponibles.
 # 3. Cantidad de Inscripciones en un Curso Específico.
+# 4. Cantidad de Profesores con Correos que Contengan 'gmail'.
+# 5. Cantidad de Inscripciones con Calificación Mayor a 80.
 
-# Ejercicios para SUM.
+# Ejercicios para SUM
 
-# 4. Suma de Calificaciones en un Curso.
-# 5. Suma de Calificaciones para Estudiantes con Calificación Mayor a 80.
-# 6. Suma Total de Calificaciones en Todos los Cursos.
+# 6. **Suma de Calificaciones en un Curso.
+# 7. **Suma de Calificaciones para Estudiantes con Calificación Mayor a 80.
+# 8. **Suma Total de Calificaciones en Todos los Cursos.
+# 9. **Suma de Calificaciones en los Primeros 5 Cursos.
+# 10. **Suma de Calificaciones en Cursos con ID entre 10 y 15.
 
-# Ejercicios para AVG.
+# Ejercicios para AVG
 
-# 7. Promedio de Calificaciones en un Curso Específico.
-# 8. Promedio General de Calificaciones.
-# 9. Promedio de Calificaciones para Estudiantes con Más de 90 Puntos.
+# 11. Promedio de Calificaciones en un Curso Específico.
+# 12. Promedio General de Calificaciones.
+# 13. Promedio de Calificaciones para Estudiantes con Más de 90 Puntos.
+# 14. Promedio de Calificaciones en Cursos con Nombre que Contenga 'SQL'.
+# 15. Promedio de Calificaciones en los Primeros 3 Cursos.
 
-# Ejercicios para MIN.
+# Ejercicios para MIN
 
-# 10. Calificación Más Baja en un Curso.
-# 11. Nota Mínima General.
-# 12. Nota Más Baja para Estudiantes con Más de 80 Puntos.
+# 16. Calificación Más Baja en un Curso.
+# 17. Nota Mínima General.
+# 18. Nota Más Baja para Estudiantes con Más de 80 Puntos.
+# 19. Nota Más Baja en los Cursos con Profesor Asignado.
+# 20. Nota Más Baja en Cursos con Nombres que Contengan 'Avanzado'.
 
-# Ejercicios para MAX.
+## Ejercicios para MAX
 
-# 13. Calificación Más Alta en un Curso Específico.
-# 14. Nota Máxima General.
-# 15. Nota Más Alta para Estudiantes con Calificación Mayor a 70.
+# 21. Calificación Más Alta en un Curso Específico.
+# 22. Nota Máxima General.
+# 23. Nota Más Alta para Estudiantes con Calificación Mayor a 70.
+# 24. Nota Más Alta en los Cursos con ID entre 5 y 10.
+# 25. Nota Más Alta para Cursos con Profesor Asignado.
