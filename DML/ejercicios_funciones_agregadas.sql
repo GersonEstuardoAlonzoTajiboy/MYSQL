715cf4015061f6d3e973131b99aa975cca151221
GEAT <gersontajiboy8@gmail.com>
2025-01-09 12:20:34 -0600

    Update ejercicios_funciones_agregadas.sql

--- DML/ejercicios_funciones_agregadas.sql
+++ DML/ejercicios_funciones_agregadas.sql
@@ -1,7 +1,6 @@
-# Ejercicios con Funciones de Agregación en MySQL
+USE escuela;
 
 # Ejercicios para COUNT
-
 # 1. Cantidad de Estudiantes.
 # 2. Cantidad de Cursos Disponibles.
 # 3. Cantidad de Inscripciones en un Curso Específico.
@@ -9,33 +8,29 @@
 # 5. Cantidad de Inscripciones con Calificación Mayor a 80.
 
 # Ejercicios para SUM
-
-# 6. **Suma de Calificaciones en un Curso.
-# 7. **Suma de Calificaciones para Estudiantes con Calificación Mayor a 80.
-# 8. **Suma Total de Calificaciones en Todos los Cursos.
-# 9. **Suma de Calificaciones en los Primeros 5 Cursos.
-# 10. **Suma de Calificaciones en Cursos con ID entre 10 y 15.
+# 6. Suma de Calificaciones en un Curso.
+# 7. Suma de Calificaciones para Estudiantes con Calificación Mayor a 80.
+# 8. Suma Total de Calificaciones en Todos los Cursos.
+# 9. Suma de Calificaciones en los Primeros 5 Cursos.
+# 10. Suma de Calificaciones en Cursos con ID entre 10 y 15.
 
 # Ejercicios para AVG
-
 # 11. Promedio de Calificaciones en un Curso Específico.
 # 12. Promedio General de Calificaciones.
 # 13. Promedio de Calificaciones para Estudiantes con Más de 90 Puntos.
-# 14. Promedio de Calificaciones en Cursos con Nombre que Contenga 'SQL'.
+# 14. Promedio de Calificaciones en Cursos con ID entre 1 y 5.
 # 15. Promedio de Calificaciones en los Primeros 3 Cursos.
 
 # Ejercicios para MIN
-
 # 16. Calificación Más Baja en un Curso.
 # 17. Nota Mínima General.
 # 18. Nota Más Baja para Estudiantes con Más de 80 Puntos.
-# 19. Nota Más Baja en los Cursos con Profesor Asignado.
-# 20. Nota Más Baja en Cursos con Nombres que Contengan 'Avanzado'.
+# 19. Nota Más Baja en los Primeros 5 Cursos.
+# 20. Nota Más Baja en Cursos con ID entre 10 y 15.
 
-## Ejercicios para MAX
-
+# Ejercicios para MAX
 # 21. Calificación Más Alta en un Curso Específico.
 # 22. Nota Máxima General.
 # 23. Nota Más Alta para Estudiantes con Calificación Mayor a 70.
 # 24. Nota Más Alta en los Cursos con ID entre 5 y 10.
-# 25. Nota Más Alta para Cursos con Profesor Asignado.
+# 25. Nota Más Alta en los Primeros 5 Cursos.
